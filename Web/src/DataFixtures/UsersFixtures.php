9d68bdbbba05dd85b7ac9802aa599b98ae8c856f
SCFloki <kevin.doublet02@gmail.com>
2019-11-06 14:31:08 +0100

    Fixtures BDD users

---
+++ Web/src/DataFixtures/UsersFixtures.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use App\Entity\Utilisateurs;
+
+class UsersFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        for($i = 1; $i <= 10; $i++){
+            $user = new Utilisateurs();
+            $user->setNom("Yveh")
+                 ->setPrenom("StLaureh")
+                 ->setMail("jean.edouard@yahoo.be")
+                 ->setMdp("srgbnty89S")
+                 ->setLocalisation("croutte sur marne");
+
+                 $manager->persist($user);
+        }
+
+        $manager->flush();
+    }
+}
