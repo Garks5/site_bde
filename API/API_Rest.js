343fbd73b18558bc9834253833231285b86f57e9
Garks5 <lucas.duleu@hotmail.fr>
2019-11-06 15:41:28 +0100

    API GET

--- API/API_Rest.js
+++ API/API_Rest.js
@@ -1,6 +1,6 @@
-//L'application requiert l'utilisation du module Express.
-//La variable express nous permettra d'utiliser les fonctionnalités du module Express.
+var bdd = require('./BDDConnect');
 var express = require('express');
+
 // Nous définissons ici les paramètres du serveur.
 var hostname = 'localhost';
 var port = 3000;
@@ -12,12 +12,10 @@ var app = express();
 //C'est à partir de cet objet myRouter, que nous allons implémenter les méthodes.
 var myRouter = express.Router();
 
-// Je vous rappelle notre route (/piscines).
 myRouter.route('')
-// J'implémente les méthodes GET, PUT, UPDATE et DELETE
 // GET
 .get(function(req,res){
-	  res.json({message : "Liste toutes les piscines de Lille Métropole", methode : req.method});
+            res.json(bdd.queryGET._results[0]); 
 })
 //POST
 .post(function(req,res){
@@ -28,14 +26,14 @@ myRouter.route('')
       res.json({message : "Mise à jour des informations d'une piscine dans la liste", methode : req.method});
 })
 //DELETE
-.delete(function(req,res){
-res.json({message : "Suppression d'une piscine dans la liste", methode : req.method});
-});
-
+.delete(function(req,res){ 
+      res.json({message : "Suppression d'une piscine dans la liste", methode : req.method});  
+      }); 
+       
 // Nous demandons à l'application d'utiliser notre routeur
-app.use(myRouter);
-
-// Démarrer le serveur
+app.use(myRouter);  
+       
+// Démarrer le serveur 
 app.listen(port, hostname, function(){
-	console.log("Mon serveur fonctionne sur http://"+ hostname +":"+port);
+      console.log("Mon serveur fonctionne sur http://"+ hostname +":"+port); 
 });
