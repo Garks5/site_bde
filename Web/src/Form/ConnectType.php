a090be0088b03df7f066900c6dbe71238a372517
Pauline2602 <paulinelegroux@laposte.net>
2019-11-11 16:13:21 +0100

    bouton type

--- Web/src/Form/ConnectType.php
+++ Web/src/Form/ConnectType.php
@@ -7,6 +7,7 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
 class ConnectType extends AbstractType
 {
@@ -16,7 +17,7 @@ class ConnectType extends AbstractType
         //ajout d'une zone de text qui correspondra au mail : EmailType
         //elle ne peut pas être vide
             ->add('mail', EmailType::class, array('label'=> 'Adresse Email', 'required' => true, 'attr' => array('class'=>'form-group', 'placeholder'=>'Entrez votre adresse mail')))
-            ->add('mdp', TextType::class, array('label'=> 'Mot de passe', 'required' => true, 'attr' => array('class'=>'form-group', 'placeholder'=>'Entrez votre mot de passe')))
+            ->add('mdp', PasswordType::class, array('label'=> 'Mot de passe', 'required' => true, 'attr' => array('class'=>'form-group', 'placeholder'=>'Entrez votre mot de passe')))
             ->add('submit', SubmitType::class, array('label'=>"Se connecter", 'attr' => array('class'=>'btn')))
         ;
     }
