4f76dc1cca3a4be3f2f16afdaf92b1ce552e4d3c
Pauline2602 <paulinelegroux@laposte.net>
2019-11-13 11:09:02 +0100

    carrousel

--- Web/src/Controller/BoutiqueController.php
+++ Web/src/Controller/BoutiqueController.php
@@ -25,10 +25,27 @@ class BoutiqueController extends AbstractController
         curl_close($ch);
         $return = json_decode($return, true);
        // return var_dump($return);
-        $data=$return;
+      
+       $cht = curl_init();
+       curl_setopt($cht, CURLOPT_URL, 'localhost:3000/boutique');
+       curl_setopt($cht, CURLOPT_RETURNTRANSFER, true);
+       curl_setopt($cht, CURLOPT_CUSTOMREQUEST, "GET");
+       $topreturn = curl_exec($cht);
+       curl_close($cht);
+       $topreturn = json_decode($topreturn, true);
+       $img1=$topreturn[0]['picture'];
+       $img2=$topreturn[1]['picture'];
+       $img3=$topreturn[2]['picture'];
+       // return var_dump($return);
+      
+         //return var_dump($img2);
+      
         return $this->render('main/boutique.html.twig', [
             'controller_name' => 'BoutiqueController',
-            'articles' =>$data
+            'articles' =>$return, 
+            'top1'=>$img1,
+            'top2'=>$img2,
+            'top3'=>$img3
         ]);
     }
 
