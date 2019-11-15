48c3f4acab5e3adf7cc78d4adfd708e77135db23
Pauline2602 <paulinelegroux@laposte.net>
2019-11-15 11:31:05 +0100

    CSS

--- Web/src/Form/pictureType.php
+++ Web/src/Form/pictureType.php
@@ -12,7 +12,7 @@ class pictureType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-        //ajout d'une zone de text qui correspondra au mail : EmailType
+        //ajout d'une zone de texte qui correspondra à un fbouton qui permet d'ajouter des fichiers : FileType
         ->add('picture', FileType::class, array('label'=> 'Ajouter une photo', 'required' => true,'attr' => array('class'=>'btn btn-primary')))  
         ->add('submit', SubmitType::class, array('label'=>"Valider", 'attr' => array('class'=>'btn btn-primary')))
         ;
